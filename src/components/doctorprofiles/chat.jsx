ccb565363b6e34d3f3585f1ecef94b3dacba9968
haseebkhanak <saadkhanak13@gmail.com>
2024-09-04 23:00:57 +0500

    Files Added

--- src/components/doctorprofiles/chat.jsx
+++ src/components/doctorprofiles/chat.jsx
@@ -1,11 +1,16 @@
-import { set } from "mongoose";
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { io } from "socket.io-client"
 
 const socket = io('http://localhost:3000');
 
 export default function ChatRoom() {
     const [message, setMessage] = useState('')
+    const [storeMessages, setstoreMessages] = useState([])
+    const lastMessageRef = useRef(null);
+
+    useEffect(() => {
+        lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+    }, [storeMessages]);
 
     const messagehandler = (event) => {
         setMessage(event.target.value)
@@ -13,12 +18,11 @@ export default function ChatRoom() {
 
     const submitMessage = (event) => {
         event.preventDefault();
-        
-        if(message){
+
+        if (message) {
             socket.emit('message', message);
-            document.querySelector('.showchat').style.display="block"
-            document.querySelector('.showchat').innerHTML += `<div class="chat-message">${message}</div>`
-            document.querySelector('.chatinput').value = ''
+            setstoreMessages([...storeMessages, message])
+            setMessage('')
         }
         if (!message) {
             alert("Type Message")
@@ -30,18 +34,26 @@ export default function ChatRoom() {
 
     return (
         <>
-            <div className="showchat" style={{ position: "absolute" }}>
+            <div className="showchat" style={{position:"absolute",overflowY: "auto", maxHeight: "400px" }}>
+
+                {storeMessages && storeMessages.map((msg,index) =>
+                    <div key={index} className="chat-message">
+                        {msg}
+                    </div>)}
+
+            <div ref={lastMessageRef}></div>
             </div>
+
 
             <form action="" onSubmit={submitMessage}>
                 <div className="flex">
                     <div>
-                    <input type="textarea" className="chatinput" onChange={messagehandler} value={message} placeholder="Type..."/>
+                        <input type="textarea" className="chatinput" onChange={messagehandler} value={message} placeholder="Type..." />
                     </div>
-<div style={{ marginTop: "550px",marginLeft:"20px",width:"60px",backgroundColor:"white",boxShadow: "5px 5px 20px rgb(107, 102, 102)",borderRadius:"20px"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8" onClick={submitMessage} style={{marginLeft:"16px",marginTop:"7px",cursor:"pointer"}}>
-                        <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
-                    </svg>
+                    <div style={{ marginTop: "550px", marginLeft: "20px", width: "60px", backgroundColor: "white", boxShadow: "5px 5px 20px rgb(107, 102, 102)", borderRadius: "20px" }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8" onClick={submitMessage} style={{ marginLeft: "16px", marginTop: "7px", cursor: "pointer" }}>
+                            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+                        </svg>
                     </div>
                 </div>
 
