65e093534769e966af5e9d491c00e577eb7e1aff
haseebkhanak <saadkhanak13@gmail.com>
2024-11-27 23:58:07 +0500

    Files Added

--- src/components/prediction.jsx
+++ src/components/prediction.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 
 const MLIntegration = () => {
   const [features, setFeatures] = useState("");
-  const [prediction, setPrediction] = useState(null);
+  const [diseasePrediction, setPrediction] = useState(null);
+  const [recommendedMedicines, setRecommendation] = useState(null);
+  const [prescription, setPrescription] = useState(null);
 
   const handleSubmit = async (e) => {
     console.log(features)
@@ -14,21 +16,66 @@ const MLIntegration = () => {
       return
     }
 
-    if(features.length<3)
-      {
-        alert("Select more than two symptoms")
-        return
-      }
+    // if(features.length<3)
+    //   {
+    //     alert("Select more than two symptoms")
+    //     return
+    //   }
     
     try {
       const featuresArray = features
-      const response = await axios.post("http://localhost:8000/predict", {
+      const response = await axios.post("http://localhost:4000/predict", {
         features:featuresArray
       });
-      setPrediction(response.data.prediction);
+      setPrediction(response.data.diseasePrediction);
+      setRecommendation(response.data.recommendedMedicines);
+
+      if(response.data.diseasePrediction==="COVID"){
+        setPrescription(`For COVID-19, initiate antiviral treatment for hospitalized patients requiring oxygen support,
+         and provide corticosteroids to reduce inflammation; ensure supportive care with hydration;
+         monitor for worsening symptoms; recommend over-the-counter medications for fever and pain management;
+         and advise on the use of a pulse oximeter to track oxygen levels at home.`)
+        return
+      }
+  
+      else if(response.data.diseasePrediction==="MALARIA"){
+        setPrescription(`For malaria, initiate appropriate antimalarial therapy based on severity and type of malaria;
+         ensure treatment is taken with food for better absorption;
+         educate the patient on prevention measures such as the use of insecticide-treated bed nets;
+         and instruct on recognizing signs of complications that require immediate medical attention.`)
+         return
+      }
+
+      else if(response.data.diseasePrediction==="IMFLUENZA"){
+        setPrescription(`For influenza, begin antiviral treatment if symptoms started within the last 48 hours;
+         encourage rest and hydration; recommend supportive care measures to relieve symptoms;
+         advise on over-the-counter medications for fever and discomfort;
+         and stress the importance of annual vaccinations for prevention.`)
+         return
+      }
+
+      else if(response.data.diseasePrediction==="PNEUMONIA"){
+        setPrescription(`For pneumonia, initiate appropriate antibiotic therapy based on the suspected causative agent;
+         provide supportive care with hydration and rest; monitor for respiratory distress;
+         advise on cough management techniques; and schedule follow-up evaluations
+         to assess recovery and response to treatment.`)
+         return
+      }
+
+      else if(response.data.diseasePrediction==="DENGUE"){
+        setPrescription(`For dengue fever, Paracetamol is used for pain and fever, while NSAIDs and aspirin are avoided due to bleeding risks. </br>
+         Hydration is crucial, using ORS or IV fluids if necessary, along with regular fluid intake.
+         Platelet monitoring is essential to prevent complications, and severe cases may require hospitalization.
+         Patients should rest, use cool compresses for comfort, and prevent mosquito bites.
+         Emergency care is needed for severe symptoms like bleeding or abdominal pain.
+         Apple juice are recommended.`)
+         return
+      }
+
     } catch (error) {
       console.error("Error making prediction:", error);
     }
+
   };
 
   return (
@@ -165,13 +212,27 @@ className="w-4 h-4 text-green-600 border-blue-300 rounded focus:ring-blue-500 da
 <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Fatigue</label> <br />
 
 
-        <button type="submit">Predict</button>
+        <button type="submit">Predict</button> <br />
       </form>
-      {prediction && (
+      {diseasePrediction && (
         <div>
           <h2>Disease:</h2>
-          <p>{prediction}</p>
+          <p>{diseasePrediction}</p> <br />
         </div>
+      )}
+
+      {recommendedMedicines &&(
+        <div>
+        <h2>Medicines:</h2>
+        <p>{recommendedMedicines}</p> <br />
+      </div>
+      )}
+
+{prescription &&(
+        <div>
+        <h2>PRESCRIPTION:</h2>
+        <p>{prescription}</p>
+      </div>
       )}
     </div>
   );
